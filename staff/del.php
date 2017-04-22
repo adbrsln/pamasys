b57247edebe8bf191dadf43c20c348da0ccc18cc
Muhammad Adib bin Ahmad Roslan <adbroslan@gmail.com>
2017-04-22 15:54:09 +0800

    Syntax update

--- staff/del.php
+++ staff/del.php
@@ -23,10 +23,7 @@ elseif($type == 'pr'){
     $query = "DELETE FROM patient WHERE patientID = '$num'";
 		mysqli_query($connect,$query) or die ("Error Query [".$strSQL."]");
 		$query = "FLUSH PRIVILEGES";
-    $query = "DELETE FROM diagnosis WHERE pid = '$num'";
-		mysqli_query($connect,$query) or die ("Error Query [".$strSQL."]");
-		$query = "FLUSH PRIVILEGES";
-    
+   
     echo '<meta http-equiv="refresh" content="0;url=./list.php?s=t">'; 
 }
 
