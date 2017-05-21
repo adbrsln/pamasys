008f6fd21cdf9aa2bd9ee30cb11fee37f65af672
Muhammad Adib bin Ahmad Roslan <adbroslan@gmail.com>
2017-05-21 12:16:29 +0800

    update patient details

--- admin/listdiagnosis.php
+++ admin/listdiagnosis.php
@@ -1,4 +1,4 @@
- <?php 
+ <?php
 //include 'include/check.php';
 include '../include/db.php';
 $transid = $_GET["id"];
@@ -26,40 +26,42 @@ $result = mysqli_query($connect,$sql3);
                         <h1 class="page-header">
                             Patient <small>Management</small>
                         </h1>
-                        
+
                     </div>
                 </div>
-             
+
 
                <div class="row">
                     <div class="col-lg-12">
                         <div class="panel panel-default">
                             <div class="panel-heading">
-                                
+
                                 <h3 class="panel-title"><i class="fa fa-user fa-fw"></i> <a data-toggle="collapse" href="#collapse1">Patient Information</a></h3>
                             </div>
                             <div id="collapse1" class="panel-collapse collapse">
                             <div class="panel-body">
                         <?php $total_rows = 1; while($row2 = mysqli_fetch_assoc($result)){  ?>
                         <Strong>Patient Information</Strong></br></br>
+                        <form method="POST" action = "pproses.php">
                                  <label>Name</label> </br>
-                                   <input class="form-control"  type="text" name = "user" value ="<?php echo $row2['patientName']; ?>"  required>
+                                   <input class="form-control"  type="text" name = "pname" value ="<?php echo $row2['patientName']; ?>"  required>
                                      </br>
                                 <label>Identification Card No.</label> </br>
-                                           <input class="form-control"  type="text" name = "user" value ="<?php echo $row2['patientIc']; ?>" required>
+                                           <input class="form-control"  type="text" name = "pic" value ="<?php echo $row2['patientIc']; ?>" maxlength="12" required>
                                              </br>
                                 <label>Address</label> </br>
-                                           <input class="form-control"  type="text" name = "user" value ="<?php echo $row2['patientAddress']; ?>" required>
+                                           <input class="form-control"  type="text" name = "address" value ="<?php echo $row2['patientAddress']; ?>" required>
                                              </br>
                                 <label>Phone Number</label> </br>
-                                           <input class="form-control"  type="text" name = "user" value ="<?php echo $row2['patientPhoneNumber']; ?>" required>
+                                           <input class="form-control"  type="text" name = "notel" value ="<?php echo $row2['patientPhoneNumber']; ?>"maxlength="11" required>
                                              </br>
+                                             <input type="hidden" name ="pid" value = "<?php echo $row2['patientID']; ?>" >
                                    <input class="btn btn-primary" type ="submit" class  = "btn" name = "submit" id ="submit" value="Submit"></br></br>
-                                
-                            
+
+                              </form>
                         <?php } ?>
                     </div>
-  
+
                             </div>
                         </div>
                     </div>
@@ -68,12 +70,12 @@ $result = mysqli_query($connect,$sql3);
                     <div class="col-lg-12">
                         <div class="panel panel-default">
                             <div class="panel-heading">
-                                
+
                                 <h3 class="panel-title"><i class="fa fa-user fa-fw"></i>Patient Diagnosis List</h3>
                             </div>
-                           
+
                             <div class="panel-body">
-                                <div class="dataTable_wrapper">                                    
+                                <div class="dataTable_wrapper">
                                     <table class="table table-striped table-bordered table-hover" id="dataTables-example">
                                         <thead>
                                             <tr>
@@ -92,25 +94,25 @@ $result = mysqli_query($connect,$sql3);
                                                 <td><?php echo $row2['diagid']; ?></td>
                                                 <td><?php echo $row2['checkin']; ?></td>
                                                  <td><?php echo $row2['title']; ?><?php echo $row2['drname']; ?></td>
-                                                  <td><?php switch($row2['status']){ 
+                                                  <td><?php switch($row2['status']){
                                                         case 1 : echo '<span class="label label-primary">'; echo $row2['sname']; echo '</span>'; break;
                                                         case 2 : echo '<span class="label label-warning">'; echo $row2['sname']; echo '</span>'; break;
                                                         case 3 : echo '<span class="label label-success">'; echo $row2['sname']; echo '</span>'; break;
                                                         case 4 : echo '<span class="label label-danger">'; echo $row2['sname']; echo '</span>'; break;
                                                         }?></td>
-                                                
-                                                
+
+
                                                 <td ><center><a  class = "btn btn-primary btn-sm"  href="view.php?id=<?=$row2['diagid'];?>" ><span class="glyphicon glyphicon-edit" ></span> View</a>&nbsp;
                                                      <a    target="_blank"  class = "btn btn-success btn-sm"  href= "print.php?id=<?=$row2['diagid'];?>" ><span class="glyphicon glyphicon-print" aria-hidden="true"></span> Print</a>&nbsp;
-                                                    
+
                                                     <a   id = "confirmation" class = "btn btn-danger btn-sm"  href="del.php?id=<?=$row2['diagid'];?>&t=rd" ><span class="glyphicon glyphicon-remove" aria-hidden="true"></span>Delete</a></center></td>
                                               </tr>
-                                             <?php $total_rows++; }  ?> 
-                                           
+                                             <?php $total_rows++; }  ?>
+
                                         </tbody>
                                     </table>
                                     <!--- bawah ni pagination-->
-                                 	
+
                                 </div>
 
                             </div>
@@ -118,8 +120,7 @@ $result = mysqli_query($connect,$sql3);
                         </div>
                     </div>
                 </div>
-       
+
     </div>
     <!-- /.footer -->
     <?php include "include/footer.php"; ?>
-
---
+++ admin/pproses.php
@@ -0,0 +1,37 @@
+
+<?php
+include 'include/check.php';
+include '../include/db.php';
+
+
+$id = $_POST['pid'];
+$n = $_POST['pname'];
+$ic = $_POST['pic'];
+$notel = $_POST['notel'];
+$add = $_POST['address'];
+
+
+
+if($id == 0)
+{
+
+
+  echo '<meta http-equiv="refresh" content="0;url=../admin/patient.php?s=f">';
+}
+else
+    {
+
+        $query = "UPDATE patient set patientName= '$n',patientIc='$ic',patientAddress='$add',patientPhoneNumber= '$notel' where patientID = $id";
+        mysqli_query($connect,$query) or die ("Error Query");;
+        $query = "FLUSH PRIVILEGES";
+
+
+
+
+    echo '<meta http-equiv="refresh" content="0;url=../admin/patient.php?s=t">';
+
+    }
+
+
+
+?>
